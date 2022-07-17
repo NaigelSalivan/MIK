2983170174b5915dc76bbe8e14310ad1588cbe70
Naigel <rsnaigel@gmail.com>
2022-07-17 18:50:45 +1000

    split log 2

--- MikAPI.py
+++ MikAPI.py
@@ -22,12 +22,41 @@ class APImikrotik_v1:
             input = sshCli.send_command(inputcomand)
         if outputcomand.strip():
             output = sshCli.send_command(outputcomand)
-            file = open("BackUp-Config-Mikrotik-Netmiko.txt", "w")
-            getLog = output.split(" ")
-            print(getLog)
-            # file.write(getLog)
+            # file = open("BackUp-Config-Mikrotik-Netmiko.txt", "w")
+            getLog = output.split("\n")
+
+            dOutput = []
+            n=0
+
+            for elemet in getLog:
+                a = elemet.strip()
+
+                aformated = a.split(" ", 2)
+                if aformated[1] == 'pptp,ppp,info':
+                    aformated[2] = aformated[2].split(":")
+                    print(aformated)
+
+                dOutput.append(aformated)
+                # dOutput.append(a.split(" ", 2))
+
+                # print(dOutput[n][1] + " : " + dOutput[n][2])
+
+                if n != len(getLog):
+                    n += 1
+                else:
+                    break
+
+            # print(len(getLog))
+            print(dOutput)
+
+            # with open('BackUp-Config-Mikrotik-Netmiko.txt', 'w') as filehandle:
+            #     for listitem in dOutput:
+            #         filehandle.write('%s\n' % listitem)
+
+
+            # file.write(OutputTxt)
             # file.close()
-        # command = self.inputcomand
+            # command = self.inputcomand
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from MikAPI import APImikrotik_v1
 # inputcomand ="/ip firewall address-list add address=69.23.209.0/24 list=\"blok IP\""
 inputcomand =""
 # outputcomand ="/log print where message~\"pptp\""
+# outputcomand ="/log print where message~\"logged\""
 outputcomand ="/log print"
 # outputcomand =""
 connect = APImikrotik_v1(mk_ip, mk_port, mk_user, mk_psw, inputcomand, outputcomand)
