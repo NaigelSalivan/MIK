af8f25ceb9574be6a011c7ca31599406c93d6324
NaigelSalivan <rsnaigel@gmail.com>
2022-12-26 22:11:04 +1000

    test cert uer create

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from config import pg_user, pg_host, pg_port, pg_db_name, pg_password, mk_ip, mk_port, mk_user, mk_psw
 from MikAPI import APImikrotik_v1
 
-# inputcomand ="/ip firewall address-list add address=69.23.209.0/24 list=\"blok IP\""
-inputcomand =""
-# outputcomand ="/log print where message~\"pptp\""
-outputcomand ="/log print where message~\"logged\""
+cert_name = "test"
+
+command_to_mik = 'certificate/add name=' + cert_name + ' common-name=' + cert_name + ' key-usage=tls-client key-size=4096 days-valid=3650'\
+                 #+ '\ncertificate/add name=test2 common-name=test2 key-usage=tls-client key-size=4096 days-valid=3650'
+
+print (command_to_mik)
+inputcomand ="/ip firewall address-list add address=69.23.209.0/24 list=\"blok IP\""
+# outputcomand ="/log print where message~\"pptp\"" \"logged\"
+# certificate/add name=test common-name=test key-usage=tls-client key-size=4096 days-valid=3650
+#    certificate/add name=test common-name=test key-usage=tls-client key-size=4096 days-valid=3650
+outputcomand = command_to_mik #"/ip/address/print"
+command_to_mik2 = 'certificate/sign name=' + cert_name + ' ca=newCA'
+outputcomand2 = command_to_mik2
+
 # outputcomand ="/log print where topics~\"error\""
 # outputcomand =""
 connect = APImikrotik_v1(mk_ip, mk_port, mk_user, mk_psw, inputcomand, outputcomand)
