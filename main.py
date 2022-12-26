38cfcce41adc8707b89657cec7301d548d94967b
NaigelSalivan <109441763+NaigelSalivan@users.noreply.github.com>
2022-12-27 00:36:58 +1000

    add PWD generator test py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from MikAPI import APImikrotik_v1
 
 cert_name = "test"
 
-command_to_mik = 'certificate/add name=' + cert_name + ' common-name=' + cert_name + ' key-usage=tls-client key-size=4096 days-valid=3650'\
-                 #+ '\ncertificate/add name=test2 common-name=test2 key-usage=tls-client key-size=4096 days-valid=3650'
+command_to_mik = 'certificate/add name=' + cert_name + ' common-name=' + cert_name + ' key-usage=tls-client key-size=4096 days-valid=3650;'\
+                 + 'certificate/add name=test2 common-name=test2 key-usage=tls-client key-size=4096 days-valid=3650'
 
 print (command_to_mik)
 inputcomand ="/ip firewall address-list add address=69.23.209.0/24 list=\"blok IP\""
